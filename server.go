57e9191e220780fbe32a133d33b48ae254821c1e
haojie06 <haojie06@qq.com>
2021-06-07 23:12:10 +0800

    流量初步可以可视化了，并使用了网页模板

--- server.go
+++ server.go
@@ -3,37 +3,47 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"log"
+	"html/template"
 	"net/http"
 )
 
 var dataChan chan string
-
-func captureHandler(w http.ResponseWriter, req *http.Request) {
-	// 向getData协程发送信号
-
-	param := req.URL.Path[1:]
-	log.Println("接收到请求", param)
-	if param == "str" {
-		dataChan <- "getStr"
-	} else if param == "json" {
-		dataChan <- "getData"
-	} else {
-		dataChan <- " "
-	}
-	data := <-dataChan
-	fmt.Fprintf(w, data)
-}
 
 func main() {
 	var listenPort string
 	fmt.Println("请输入web服务器监听端口")
 	fmt.Scanln(&listenPort)
 	dataChan = make(chan string)
+	// ！！！注意，如果模板中用了本地的js、css文件，需要先指明文件在哪
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
 	go getData(dataChan)
-	http.HandleFunc("/", captureHandler)
+	http.HandleFunc("/", pageHandler)
+	http.HandleFunc("/str", strHandler)
+	http.HandleFunc("/json", jsonHandler)
 	err := http.ListenAndServe(":"+listenPort, nil)
 	handleErr(err)
+}
+func pageHandler(w http.ResponseWriter, req *http.Request) {
+	allowCORS(w)
+	//加载模板
+	tpl, err := template.ParseFiles("templates/index.tmpl")
+	handleErr(err)
+	err = tpl.Execute(w, "")
+	handleErr(err)
+}
+func strHandler(w http.ResponseWriter, req *http.Request) {
+	allowCORS(w)
+	dataChan <- "getStr"
+	data := <-dataChan
+	fmt.Fprintf(w, data)
+}
+
+func jsonHandler(w http.ResponseWriter, req *http.Request) {
+	allowCORS(w)
+	dataChan <- "getData"
+	data := <-dataChan
+	fmt.Fprintf(w, data)
 }
 
 // 主协程监听web端口，所以需要第三个协程与抓包协程通信并计算,以及暂时保存数据
@@ -66,6 +76,14 @@ func getData(dataChan chan string) {
 			}
 		}
 	}
+}
+
+func allowCORS(w http.ResponseWriter) {
+	// 允许跨域
+	w.Header().Set("Access-Control-Allow-Origin", "*")                                                            // 允许访问所有域，可以换成具体url，注意仅具体url才能带cookie信息
+	w.Header().Add("Access-Control-Allow-Headers", "Content-Type,AccessToken,X-CSRF-Token, Authorization, Token") //header的类型
+	w.Header().Add("Access-Control-Allow-Credentials", "true")                                                    //设置为true，允许ajax异步请求带cookie信息
+	w.Header().Add("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")                             //允许请求方法
 }
 
 // 将bandwidthList 转为JSON
